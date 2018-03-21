9f1fd5595d3c6f217c3e00b486e250e42e1db6fd
Travis Douce <travis.douce@stitchfix.com>
2018-03-20 19:05:26 -0700

    Fix bug: tally score

--- space_invaders.rb
+++ space_invaders.rb
@@ -62,25 +62,29 @@ class SpaceInvaders < Gosu::Window
           @alien_lasers << alien.shoot_laser
         end
       end
+    end
 
+    @font.draw("Health: #{ player.health }", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    @font.draw("Points: #{ player.tally_score(@aliens) }", 10, 35, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+
+    unless @level.won?
       @aliens = @aliens.reject do |alien|
         if player.hit_alien?(alien)
+
           @level.inc_kill_count
+          puts "Kill count: #{ @level.kill_count }"
           true
         else
           false
         end
       end
-    end
 
-    if @aliens.length <= @level.repopulation_threshold
-      @level.aliens.each do |alien|
-        @aliens << alien
+      if @aliens.length <= @level.repopulation_threshold
+        @level.aliens.each do |alien|
+          @aliens << alien
+        end
       end
     end
-
-    @font.draw("Health: #{ player.health }", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    @font.draw("Points: #{ player.tally_score(@aliens) }", 10, 35, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
 
     if player.dead?
       @font.draw("GAME OVER", 315, 225, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
